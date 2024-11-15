b94939090d83989af6cb01133aff9af0bfb7b122
arthurmalangin <amalangin@icloud.com>
2024-11-15 09:16:54 +0100

    i hope last commit

--- srcs/requirements/nginx/conf/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-until nc -z wordpress 8010; do
-  echo "Waiting for WordPress to be ready..."
-  sleep 2
-done
-
-nginx -g 'daemon off;'
--- srcs/requirements/wordpress/tools/wp_cli_install.sh
+++ srcs/requirements/wordpress/tools/wp_cli_install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-#
 sleep 10
 
-wp core download --path="/var/www/html" --allow-root
-wp config create --path="/var/www/html" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
-wp core install --path="/var/www/html" --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
-wp user create --path="/var/www/html" --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASS --role='contributor'
+if [ ! -f "/var/www/html/wp-config.php" ]
+then
+	wp core download --path="/var/www/html" --allow-root
+	wp config create --path="/var/www/html" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
+	wp core install --path="/var/www/html" --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+	wp user create --path="/var/www/html" --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASS --role='contributor'
+else
+	echo "nopp"
+
+fi
+
 
 #exec finale voir Dockerfile pour plus de details
 exec /usr/sbin/php-fpm7.4 -F
